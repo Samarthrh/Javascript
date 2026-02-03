e7083210b24ea4754e8d125ad8230d5be33e7101
Samarthrh <138220668+Samarthrh@users.noreply.github.com>
2026-02-03 15:29:31 +0000

    conersion of datatypes

---
+++ js_basics/03_ConversionOperation.js
@@ -0,0 +1,38 @@
+let score = false
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let conversion = Number(score)
+console.log(typeof conversion)
+console.log(conversion)
+
+/*
+Conversion Operation:
+
+"33" => 33;
+"33new" => Nan (Not a Number)
+"true" => 1
+"false" => 0
+ null  =>  null
+ undefined =>  undefined
+ */
+console.log("------------------")
+ let isLoggedIn = ""
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn)
+
+/* 
+1 => true
+0 => false
+"" => false
+"Samarth third feb" => true
+*/
+console.log("------------------")
+let someNumber = 8
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber)
+
+
+
