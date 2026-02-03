dca9eee1219f7dc009893b9150f6eb71c0760a2d
Samarthrh <138220668+Samarthrh@users.noreply.github.com>
2026-02-03 16:24:14 +0000

    performing operations

--- js_basics/03_ConversionOperation.js
+++ js_basics/03_ConversionOperation.js
@@ -1,11 +1,11 @@
-let score = false
+// let score = false
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
-let conversion = Number(score)
-console.log(typeof conversion)
-console.log(conversion)
+// let conversion = Number(score)
+// console.log(typeof conversion)
+// console.log(conversion)
 
 /*
 Conversion Operation:
@@ -17,10 +17,11 @@ Conversion Operation:
  null  =>  null
  undefined =>  undefined
  */
-console.log("------------------")
- let isLoggedIn = ""
-let booleanLoggedIn = Boolean(isLoggedIn)
-console.log(booleanLoggedIn)
+
+// console.log("------------------")
+//  let isLoggedIn = ""
+// let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn)
 
 /* 
 1 => true
@@ -28,11 +29,49 @@ console.log(booleanLoggedIn)
 "" => false
 "Samarth third feb" => true
 */
-console.log("------------------")
-let someNumber = 8
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber)
+
+// console.log("------------------")
+// let someNumber = 8
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+// console.log(stringNumber)
+
+// *********************Operations************************
+
+// let value = 8
+// let negValue = -value
+// console.log(typeof negValue)
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(10/2)
+// console.log(10%3)
+
+// let str1 = "Hello"
+// let str2 = " Samarth"
+// console.log(str1+str2)
+
+// console.log("1" + 2)
+// console.log(2 + "1")
+// console.log(2 + 2 + "1")
+// console.log("3" + 2 + 7)
+// console.log(4 + 7 + 9 / 2 - 1 * 13 + " Samarth")
+// console.log(+true)
+// console.log(+false)
+// console.log(+"false")
+// console.log(undefined + null)
+
+let counter = 100
+counter++;
+console.log(counter);
+
+// ********************************************
+
+
+
 
 
 
