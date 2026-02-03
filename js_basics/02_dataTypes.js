b8d7132ff5287b646f08218684a60dbbf2e053e2
Samarthrh <138220668+Samarthrh@users.noreply.github.com>
2026-02-03 14:43:12 +0000

    datatype

---
+++ js_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" //treat code as new version of JavaScript
+
+console.log("Hi Samarth")
+
+// alert(3 + 6) generates an error 
+
+console.log(3 + 3);
+
+let name = "Vrund"
+let age = 19
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint => larger than 2 to power 53 
+// string => "new"
+// boolean => "true" OR "false"
+// null
+// undefined => value not assigned
+// symbol => unique
+// object 
+
+console.log(typeof "Vrund")
+
+console.log(typeof age)
+console.log(typeof null) // bug in JS null => returns object
+console.log(typeof undefined) // returns undefined
+
